d6d86bad6bf73958aae9e8c8cbba8ef9f4857341
shuklaaradhana <aradhana.shukla@sjsu.edu>
2013-12-03 17:07:21 -0800

    Adding searchObject domain file.

---
+++ dropbox/src/main/java/edu/sjsu/cmpe/dropbox/domain/searchObject.java
@@ -0,0 +1,27 @@
+package edu.sjsu.cmpe.dropbox.domain;
+
+public class searchObject {
+
+	private String id;
+	private String label;
+	private String value;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
